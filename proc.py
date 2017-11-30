d7d5ba9a5980351cb6b3dc7d93d9986052a9da7b
Michel Vuijlsteke from Cthulhu <michel@zog.org>
2017-11-30 23:24:53 +0100

    initial

--- proc.py
+++ proc.py
@@ -1,27 +1,31 @@
-import sys, pygame, random
+"""
+Ik dacht, ik probeer eens iets te maken dat dingen op het scherm toont.
+"""
+
+import sys
+import random
+import pygame
+
+
 pygame.init()
 
-size = width, height = 800, 600
+SIZE = WIDTH, HEIGHT = 800, 600
 
-black = 0, 0, 0
-white = 255,255,255
-x=y=0
+BLACK = 0, 0, 0
+WHITE = 255, 255, 255
 
-screen = pygame.display.set_mode(size)
-clock = pygame.time.Clock()
-running = True
+SCREEN = pygame.display.set_mode(SIZE)
+CLOCK = pygame.time.Clock()
+RUNNING = True
 
-while running:
+while RUNNING:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
 
-    screen.fill(white)
-    
-    x=int(random.uniform(0,width))
-    y=int(random.uniform(0,height))
-    
-    for x in range(300,400):
-        print(x)
-        screen.set_at((x,x),black)
-    
+    SCREEN.fill(WHITE)
+
+    for x in range(1, 10000):
+        SCREEN.set_at((int(random.uniform(0, WIDTH)), int(random.uniform(0, HEIGHT))), BLACK)
+
     pygame.display.flip()
